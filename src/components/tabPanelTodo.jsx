2d4f728696d43f13d042c98a6e3ac64d447e4797
Alexandr <60702539+Alexandrjob@users.noreply.github.com>
2022-11-03 22:39:22 +0700

    Переписан код, теперь он красивенький. (#6)

--- src/components/tabPanelTodo.jsx
+++ src/components/tabPanelTodo.jsx
@@ -27,9 +27,9 @@ function TabPanelTodo(props) {
         props.data.map((item, index) =>
             <TabPanel key={(index + 1).toString()} value={(index + 1).toString()}>
                 <List dense sx={list} key={index}>
-                    <TodoItem keyy={item[0].id} name={index + ' ' + item[0].id} value={1} date={item[0].date} waiterName={item[0].waiterName} disabled={disabledItem} checked={item[0].status} handleChange={props.handleChangeCheckBox} />
-                    <TodoItem keyy={item[1].id} name={index + ' ' + item[1].id} value={2} date={item[1].date} waiterName={item[1].waiterName} disabled={disabledItem} checked={item[1].status} handleChange={props.handleChangeCheckBox} />
-                    <TodoItem keyy={item[2].id} name={index + ' ' + item[2].id} value={3} date={item[2].date} waiterName={item[2].waiterName} disabled={disabledItem} checked={item[2].status} handleChange={props.handleChangeCheckBox} />
+                    <TodoItem keyy={item[0].id} name={index + ' ' + 0} value={1} date={item[0].date} waiterName={item[0].waiterName} disabled={disabledItem} checked={item[0].status} handleChange={props.handleChangeCheckBox} />
+                    <TodoItem keyy={item[1].id} name={index + ' ' + 1} value={2} date={item[1].date} waiterName={item[1].waiterName} disabled={disabledItem} checked={item[1].status} handleChange={props.handleChangeCheckBox} />
+                    <TodoItem keyy={item[2].id} name={index + ' ' + 2} value={3} date={item[2].date} waiterName={item[2].waiterName} disabled={disabledItem} checked={item[2].status} handleChange={props.handleChangeCheckBox} />
                 </List>
             </TabPanel>
         )
