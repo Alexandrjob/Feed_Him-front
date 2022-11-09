d17db76f46b2c2d674fb409964a6984db2d520ba
Alexandr Savarskiy <sshkajob@gmail.com>
2022-11-09 23:36:40 +0700

    Добавлена динамическая генерация. Теперь количество порций генерируется исходя из полученных данных.

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -5,40 +5,35 @@ import {
 } from '@mui/material';
 import TodoCheckBox from './todoCheckBox';
 
-class TodoItem extends React.Component {
-    render() {
-        const labelTextId = `checkbox-list-secondary-label-${this.props.keyy}`;
-        let textSecondary;
-        if (this.props.date == null || this.props.waiterName == null) {
-            textSecondary = "Не покормлено";
-        }
-        else {
-            var date = getFormatString(this.props.date);
-            textSecondary = date + ' ' + this.props.waiterName;
-        }
-        return (
-            <ListItem
-                key={this.props.keyy}
-                secondaryAction={
-                    <TodoCheckBox checked={!!this.props.checked}
-                        name={this.props.name}
-                        disabled={this.props.disabled}
-                        labelTextId={labelTextId}
-                        handleChange={this.props.handleChange} />
-                }
-                disablePadding>
-                <ListItemText
-                    id={labelTextId}
-                    primary={`${this.props.value} прием`}
-                    secondary={textSecondary}
-                />
-            </ListItem>
-        );
-    }
-}
+export default function TodoItem(props) {
+    const getFormatString = (dateStr) => {
+        let date = new Date(dateStr).toLocaleTimeString().slice(0, -3);
+        return date;
+    };
 
-function getFormatString(dateStr) {
-    let date = new Date(dateStr).toLocaleString().slice(0, -3);
-    return date;
+    let textSecondary;
+    if (props.date == null || props.waiterName == null) {
+        textSecondary = "Не покормлено";
+    }
+    else {
+        var date = getFormatString(props.date);
+        textSecondary = date + ' ' + props.waiterName;
+    }
+
+    return (
+        <ListItem
+            secondaryAction={
+                <TodoCheckBox checked={!!props.checked}
+                    name={props.name}
+                    disabled={props.disabled}
+                    handleChange={props.handleChange} />
+            }
+            disablePadding>
+            <ListItemText
+                primary={`${props.servingNumber} прием`}
+                secondary={textSecondary}
+            />
+        </ListItem>
+    );
+
 }
-export default TodoItem;
